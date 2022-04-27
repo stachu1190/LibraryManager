e36b49d9be3518eba5d96171f427ebd9d8604501
stachu1190 <stachu1190@gmail.com>
2022-04-27 22:14:24 +0200

    Created Client table

---
+++ src/main/java/com/example/libraryMS/Client.java
@@ -0,0 +1,114 @@
+package com.example.libraryMS;
+
+import javax.persistence.*;
+
+@Entity(name = "Client")
+@Table(
+        name = "client",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "email_unique", columnNames = "email"),
+                @UniqueConstraint(name = "phone_number_unique", columnNames = "phone_number")
+        }
+)
+public class Client {
+
+    @Id
+    @SequenceGenerator(
+            name = "client_sequence",
+            sequenceName = "client_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "client_sequence"
+    )
+    @Column(
+            name = "id_client",
+            updatable = false
+    )
+    private Long id;
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String firstName;
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String lastName;
+    @Column(
+            name = "email",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String email;
+    @Column(
+            name = "phone_number",
+            columnDefinition = "TEXT"
+    )
+    private String phoneNumber;
+
+    public Client() {}
+
+    public Client(Long id, String firstName, String lastName, String email, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
